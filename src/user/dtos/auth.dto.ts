f22d75642a2451562fb79fb9ceae1a219541b67c
seshotake <seshotake@gmail.com>
2023-08-28 23:52:15 +0300

    Add basic signup method

---
+++ src/user/dtos/auth.dto.ts
@@ -0,0 +1,29 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+
+export class SignUpDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/, {
+    message: 'phone must be a valid phone number',
+  })
+  phone: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  password: string;
+}
