a5b7d5190ea04fb9b6fce45be4c0a88ee6a37500
seshotake <seshotake@gmail.com>
2023-08-31 16:41:11 +0300

    Add api for get current user

--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   ParseEnumPipe,
   Post,
@@ -9,6 +10,7 @@ import {
 import { GenerateProductKeyDto, SignInDto, SignUpDto } from '../dtos/auth.dto';
 import { AuthService } from './auth.service';
 import { UserType } from '@prisma/client';
+import { User, UserInfo } from '../decorators/user.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -43,5 +45,10 @@ export class AuthController {
   @Post('/key')
   generateProductKey(@Body() { email, userType }: GenerateProductKeyDto) {
     return this.authService.generateProductKey(email, userType);
+  }
+
+  @Get('/me')
+  async getCurrentUser(@User() user: UserInfo) {
+    return user;
   }
 }
